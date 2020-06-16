394522b1507be161562baf71d375fe200a981f93
Luan Azevedo <azeveedolh@gmail.com>
2020-06-15 22:40:59 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes';
+
+const server = express();
+server.use(express.json());
+
+server.use(routes);
+
+server.listen(3333, () => {
+  // eslint-disable-next-line no-console
+  console.log('Servidor iniciado na porta 3333!');
+});
